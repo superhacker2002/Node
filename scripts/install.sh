bbf158a9bd515bc030d3af0bc0603941ae15486f
0xfee1dead <58794405+xasai@users.noreply.github.com>
2024-07-12 14:18:46 +0400

    revert install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-
-set -euo pipefail
+#
 # allow specifying different destination directory
 DIR="${DIR:-"$HOME/.local/bin"}"
 
-RELEASE="${1:-"latest"}"
+# map different architecture variations to the available binaries
+ARCH=$(uname | tr [:upper:] [:lower:])
 
 # prepare the download URL
-TAG=$(curl -L -s -H 'Accept: application/json' https://github.com/denetpro/node/releases/$RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-
-ARCH=$(uname -m)
-case "$ARCH" in
-    x86_64) ARCH="amd64" ;;
-    aarch64|arm64) ARCH="arm64" ;;
-    *) error "Unsupported architecture: $ARCH" ;;
-esac
-
-
-GITHUB_FILE=$(echo "denode-$(uname -s)-$ARCH" | tr '[:upper:]' '[:lower:]')
-GITHUB_URL="https://github.com/denetpro/node/releases/download/${TAG}/${GITHUB_FILE}"
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/denetpro/node/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_FILE="denode-$ARCH.zip"
+GITHUB_URL="https://github.com/denetpro/node/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
 
 # install/update the local binary
-curl -L -o denode $GITHUB_URL
+curl -L -o denode.zip $GITHUB_URL
+unzip -a denode.zip && rm -rf denode.zip
+mkdir -p $DIR
 install -m 555 denode -t "$DIR"
-rm -f denode
 echo "`denode -v` successfully installed"
